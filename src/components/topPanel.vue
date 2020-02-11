8f0429d13e6b89e586a225a7bbc30415eb46e0fc
Alexey Onikov <orbenaixasej@gmail.com>
2020-02-11 18:12:12 +0300

    Redesign topPanel & welcome BEM

--- src/components/topPanel.vue
+++ src/components/topPanel.vue
@@ -1,14 +1,34 @@
 <template>
   <div class='top-panel'>
+    <img 
+      src='@/assets/img/128/clock-icon.png'
+      class='top-panel__icon top-panel__icon_clock'
+    >
+    <img 
+      src='@/assets/img/128/bomb-icon.png'
+      class='top-panel__icon top-panel__icon_bomb'
+    >
     <div 
-      class='restart-button'
+      class='top-panel__button top-panel__button_settings'>
+    </div>
+    <h2 class='top-panel__counter top-panel__counter_bombs'>
+      {{ $store.state.settings.bombsNumber }}
+    </h2>
+    <div 
+      class='top-panel__button'
       :class='{
-        "restart-button_game": $store.state.stage == "game",
-        "restart-button_losing": $store.state.stage == "losing",
-        "restart-button_win": $store.state.stage == "win"
+        "top-panel__button_thinking": $store.state.stage == "game",
+        "top-panel__button_angry": $store.state.stage == "losing",
+        "top-panel__button_cool": $store.state.stage == "win"
       }'
       @click='restart'
     >
+    </div>
+    <h2 class='top-panel__counter top-panel__counter_time'>
+      0
+    </h2>
+    <div 
+      class='top-panel__button top-panel__button_ref'>
     </div>
   </div>
 </template>
@@ -34,33 +54,64 @@ export default {
 <style scoped>
   .top-panel {
     height: 70px;
+    background-color: #2196F3;
+    box-shadow: inset 0 -5px #1976D2;
     display: flex;
     align-items: center;
     justify-content: center;
-    background-color: #2196F3;
-    box-shadow: inset 0 -5px #1976D2;
   }
 
-  .restart-button {
+  .top-panel__icon {
+    width: 50px;
+    height: 50px;
+    position: absolute;
+  }
+
+  .top-panel__icon_clock {
+    transform: translate(20px)
+  }
+
+  .top-panel__icon_bomb { 
+    transform: translate(-20px)
+  }
+
+  .top-panel__button {
     width: 40px;
     height: 40px;
-    transition: all 0.3s;
+    position: relative;
+    transition: background-image .3s;
   }
 
-  .restart-button:hover {
-    width: 45px;
-    height: 45px;
-  }
-
-  .restart-button_game {
+  .top-panel__button_thinking {
     background: no-repeat center/100% url(~@/assets/img/128/thinking.png);
   }
   
-  .restart-button_losing {
+  .top-panel__button_angry {
     background: no-repeat center/100% url(~@/assets/img/128/angry.png);
   }
 
-  .restart-button_win {
+  .top-panel__button_cool {
     background: no-repeat center/100% url(~@/assets/img/128/cool.png);
+  }
+
+  .top-panel__button_settings {
+    background: no-repeat center/100% url(~@/assets/img/128/settings.png);
+  }
+
+  .top-panel__button_ref {
+    background: no-repeat center/100% url(~@/assets/img/128/ref.png);
+  }
+
+  .top-panel__counter {
+    max-width: 60px;
+    margin: 20px;
+    position: relative;
+    font-family: Arial;
+    color: #303130;
+    flex-grow: 1;
+  }
+
+  .top-panel__counter_bombs {
+    text-align: right;
   }
 </style>
