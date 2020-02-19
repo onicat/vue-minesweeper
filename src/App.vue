f38706c28915a6f62759bcfd9b486b7d3c4f9a54
Alexey Onikov <orbenaixasej@gmail.com>
2020-02-19 18:05:32 +0300

    remove selectstart

--- src/App.vue
+++ src/App.vue
@@ -32,6 +32,9 @@
       window.addEventListener('contextmenu', (event) => {
         event.preventDefault()
       });
+      window.addEventListener('selectstart', (event) => {
+        event.preventDefault()
+      });
     }
   }
 </script>
